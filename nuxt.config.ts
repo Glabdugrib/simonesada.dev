93412361e5c74a6c84fb91fbd85c4ba930c7b317
Simone Sada <si.sada93@gmail.com>
2025-04-23 15:02:18 +0200

    chore: install Prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
   vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
   },
-})
+});
