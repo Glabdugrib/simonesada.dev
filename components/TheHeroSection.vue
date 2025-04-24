f301bd1e82dc819b9407e558aab03b8a21203ccb
Simone Sada <si.sada93@gmail.com>
2025-04-24 12:44:30 +0200

    feat: add hero section

---
+++ components/TheHeroSection.vue
@@ -0,0 +1,30 @@
+<template>
+   <section class="bg-white">
+      <div
+         class="mx-auto grid max-w-screen-xl px-4 py-8 lg:grid-cols-12 lg:gap-8 lg:py-16 xl:gap-0"
+      >
+         <div class="mr-auto place-self-center lg:col-span-8">
+            <h2
+               class="mb-8 max-w-2xl text-center text-lg font-bold leading-none tracking-tight text-orange-400 md:text-xl xl:text-2xl"
+            >
+               Welcome to my lab
+            </h2>
+            <h1
+               class="mb-12 max-w-2xl text-center text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl"
+            >
+               Hi, I'm Simone!
+            </h1>
+            <p
+               class="mb-12 max-w-2xl text-center font-light text-gray-500 md:text-xl lg:mb-8 lg:text-2xl"
+            >
+               I'm a <strong>Full-Stack Software Engineer</strong> who's always tinkering with new
+               ideas and pushing the boundaries of what's possible in development. This portfolio is
+               where I document my creative process, discoveries, and occasional misadventures.
+            </p>
+         </div>
+         <div class="hidden lg:col-span-4 lg:mt-0 lg:flex">
+            <img src="/public/hero-image.png" alt="mockup" />
+         </div>
+      </div>
+   </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import TheHeroSection from '~/components/TheHeroSection.vue';
+</script>
+
+<template>
+   <TheHeroSection />
+</template>
