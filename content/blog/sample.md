8313117616fd2ba117c08d35f6e070152ae943a1
Simone Sada <si.sada93@gmail.com>
2025-06-23 08:49:01 +0200

    chore: add blog post example

---
+++ content/blog/sample.md
@@ -0,0 +1,15 @@
+# Sample page
+
+Lorem ipsum
+
+## Sezione 1
+
+aaa
+
+### Sezione 1.1
+
+bbb
+
+## Sezione 2
+
+ccc
---
+++ pages/blog/[slug].vue
@@ -0,0 +1,12 @@
+<script setup>
+const slug = useRoute().params.slug;
+const { data: post } = await useAsyncData(`blog-${slug}`, () =>
+   queryCollection('blog').path(`/blog/${slug}`).first(),
+);
+console.log('SLUG:', slug);
+console.log('POST:', post);
+</script>
+
+<template>
+   <ContentRenderer :value="post" />
+</template>
