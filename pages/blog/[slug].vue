2070aa61eb61adeb332a5023dd7401d81adaf5b4
Simone Sada <si.sada93@gmail.com>
2025-06-28 12:12:40 +0200

    feat: add static header to blog page template

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,4 +1,6 @@
 <script setup>
+import dayjs from 'dayjs';
+
 const slug = useRoute().params.slug;
 const { data: post } = await useAsyncData(`blog-${slug}`, () =>
    queryCollection('blog').path(`/blog/${slug}`).first(),
@@ -8,8 +10,36 @@ console.log('POST:', post);
 </script>
 
 <template>
-   <div class="px-4 pb-16 pt-8 antialiased lg:pb-24 lg:pt-16">
+   <div class="px-4 pb-8 pt-8 antialiased lg:pb-24 lg:pt-16">
       <article class="prose lg:prose-xl mx-auto">
+         <!-- Header -->
+         <header class="mb-10">
+            <!-- Title -->
+            <h1 class="!mb-5 text-4xl font-extrabold">{{ post?.title }}</h1>
+
+            <!-- Tags -->
+            <div class="mb-2 flex flex-wrap gap-2">
+               <span
+                  v-for="tag in ['tailwind', 'ui', 'flowbite', 'prototyping']"
+                  :key="tag"
+                  class="rounded-full bg-orange-100 px-2 py-1 text-xs font-medium text-orange-600"
+               >
+                  {{ tag.toUpperCase() }}
+               </span>
+            </div>
+
+            <!-- Meta info -->
+            <div class="space-x-2 text-sm text-gray-500">
+               <span>{{ dayjs().format('MMMM D, YYYY') }}</span>
+               <span>•</span>
+               <span>5 min read</span>
+            </div>
+
+            <!-- Description -->
+            <p class="text-gray-500">{{ post?.description }}</p>
+         </header>
+
+         <!-- Content -->
          <ContentRenderer :value="post" />
       </article>
    </div>
