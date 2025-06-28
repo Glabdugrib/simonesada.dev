1ecc537c0be0c4af1d8574cd8481373a5413430b
Simone Sada <si.sada93@gmail.com>
2025-06-28 13:07:29 +0200

    feat: add overridden version of mdc's ProseCode component

---
+++ components/content/ProseCode.vue
@@ -0,0 +1,13 @@
+<template>
+   <code
+      class="inline-block rounded-md border border-stone-300 bg-stone-100 px-1.5 py-0.5 font-mono text-sm font-medium text-stone-800"
+      ><slot
+   /></code>
+</template>
+
+<style scoped>
+code::before,
+code::after {
+   content: '';
+}
+</style>
