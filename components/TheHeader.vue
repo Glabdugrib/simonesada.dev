8d45bbeb203b79c058cd888124714d640bf9af31
Simone Sada <si.sada93@gmail.com>
2025-07-07 18:50:52 +0200

    feat: add hamburger to header navbar

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -19,6 +19,32 @@ import TheLogo from './TheLogo.vue';
                </span>
             </a>
 
+            <!-- Hamburger button for mobile -->
+            <button
+               data-collapse-toggle="navbar-default"
+               type="button"
+               class="focus cursor-pointer items-center justify-center rounded-lg p-2 text-sm text-stone-600 transition-colors duration-150 hover:text-orange-600 sm:hidden"
+               aria-controls="navbar-default"
+               aria-expanded="false"
+            >
+               <span class="sr-only">Open main menu</span>
+               <svg
+                  class="h-5 w-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 17 14"
+               >
+                  <path
+                     stroke="currentColor"
+                     stroke-linecap="round"
+                     stroke-linejoin="round"
+                     stroke-width="2"
+                     d="M1 1h15M1 7h15M1 13h15"
+                  />
+               </svg>
+            </button>
+
             <!-- Navigation links for larger screens -->
             <div class="hidden items-center gap-2 sm:flex lg:order-2">
                <a
