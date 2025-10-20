b2b3a01526d5cfca6f7cac4654cdc31c73104270
Simone Sada <si.sada93@gmail.com>
2025-10-20 22:34:15 +0200

    chore(home): hide hero section

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import MarqueeDivider from '~/components/MarqueeDivider.vue';
-import TheHeroSection from '~/components/TheHeroSection.vue';
+// import MarqueeDivider from '~/components/MarqueeDivider.vue';
+// import TheHeroSection from '~/components/TheHeroSection.vue';
 
-const getRandomArbitrary = (min: number, max: number) => {
-   return Math.random() * (max - min) + min;
-};
+// const getRandomArbitrary = (min: number, max: number) => {
+//    return Math.random() * (max - min) + min;
+// };
 </script>
 
 <template>
-   <TheHeroSection />
-   <MarqueeDivider :strings="['CODE', 'PLAY', 'EXPERIMENT', 'REPEAT']" divider="✱" :speed="1" />
+   <!-- <TheHeroSection />
+   <MarqueeDivider :strings="['CODE', 'PLAY', 'EXPERIMENT', 'REPEAT']" divider="✱" :speed="1" /> -->
 </template>
