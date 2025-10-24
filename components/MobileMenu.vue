e6c0ea3b11a75a58b6b000993773549127d474e4
Simone Sada <si.sada93@gmail.com>
2025-10-24 17:23:31 +0200

    fix(header): remove transition on mobile menu position

--- components/MobileMenu.vue
+++ components/MobileMenu.vue
@@ -4,7 +4,7 @@ const props = defineProps<{ isMobileMenuOpen: boolean }>();
 
 <template>
    <div
-      class="border-warm-black fixed left-0 right-0 top-[4rem] z-10 overflow-hidden border-b-[1.5px] bg-red-200 transition-all duration-300 ease-in-out md:top-[3.5rem] lg:top-[4.5rem]"
+      class="border-warm-black fixed left-0 right-0 top-[4rem] z-10 overflow-hidden border-b-[1.5px] bg-red-200 transition-[height] duration-300 ease-in-out md:top-[3.5rem] lg:top-[4.5rem]"
       :class="{
          'h-[calc(100vh-4rem)] md:h-[calc(100vh-3.5rem)] lg:h-[calc(100vh-4.5rem)]':
             isMobileMenuOpen,
