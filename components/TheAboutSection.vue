a3790837077346bea5a914831940bf8ccdc690f7
Simone Sada <si.sada93@gmail.com>
2025-11-04 17:57:20 +0100

    feat(about): add about section w/ placeholder text & images

---
+++ components/TheAboutSection.vue
@@ -0,0 +1,46 @@
+<template>
+   <div class="gap-16 lg:grid lg:grid-cols-2">
+      <div class="text-main font-poppins font-light sm:text-lg">
+         <h2
+            class="text-main !font-poppins-rounded relative mb-6 text-center text-4xl font-extrabold tracking-tight lg:mb-7"
+         >
+            About me
+            <span
+               class="bg-accent-secondary absolute -bottom-1.5 left-1/2 h-1.5 w-16 -translate-x-1/2 rounded"
+            ></span>
+         </h2>
+         <p class="mb-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quam metus, finibus eu
+            molestie in, egestas ut tortor. Sed augue erat, dapibus sed sem tristique, condimentum
+            pulvinar nulla. Integer in interdum ante, vitae tincidunt ante. Integer mollis sapien ac
+            lorem iaculis sodales. Donec eget lacus vitae arcu efficitur dignissim.
+         </p>
+         <p class="mb-4">
+            Mauris tincidunt neque sem, congue convallis ipsum varius eu. Maecenas et posuere sem.
+            Quisque volutpat tincidunt nisi, id fringilla risus aliquam nec. Nunc maximus erat non
+            semper rutrum. Proin eleifend justo eu orci tempor auctor. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Nulla ultrices eros sit amet eros sollicitudin, vitae
+            faucibus odio scelerisque.
+         </p>
+         <p>
+            Phasellus augue urna, molestie eget felis vel, hendrerit efficitur ipsum. Donec quis
+            blandit nisl, id tincidunt nulla. Pellentesque interdum leo efficitur suscipit
+            elementum. Proin et magna volutpat, venenatis elit tempor, commodo velit.
+         </p>
+      </div>
+      <div class="mt-8 flex items-center justify-center">
+         <div class="debug grid grid-cols-2 gap-4">
+            <img
+               class="w-full rounded-lg"
+               src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png"
+               alt="office content 1"
+            />
+            <img
+               class="mt-4 w-full rounded-lg lg:mt-10"
+               src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png"
+               alt="office content 2"
+            />
+         </div>
+      </div>
+   </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 import TheHeroSection from '~/components/TheHeroSection.vue';
-// import MarqueeDivider from '~/components/MarqueeDivider.vue';
+import TheAboutSection from '~/components/TheAboutSection.vue';
+import MarqueeDivider from '~/components/MarqueeDivider.vue';
 </script>
 
 <template>
-   <TheHeroSection class="lg:mt-18 mt-16 md:mt-14" />
+   <SectionWrapper id="hero" class="bg-alt lg:mt-18 mt-16 md:mt-14">
+      <TheHeroSection />
+   </SectionWrapper>
+   <MarqueeDivider :strings="['CODE', 'PLAY', 'EXPERIMENT', 'REPEAT']" divider="✱" :speed="1" />
+   <SectionWrapper id="about" class="bg-alt">
+      <TheAboutSection />
+   </SectionWrapper>
    <div class="py-150 bg-alt"></div>
-   <!-- <MarqueeDivider :strings="['CODE', 'PLAY', 'EXPERIMENT', 'REPEAT']" divider="✱" :speed="1" /> -->
 </template>
