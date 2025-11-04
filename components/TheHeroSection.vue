a50887228430eab0e1c48925e5b4e9dbb01a1a67
Simone Sada <si.sada93@gmail.com>
2025-11-04 17:56:48 +0100

    feat(hero): apply section-wrapper to hero

--- components/TheHeroSection.vue
+++ components/TheHeroSection.vue
@@ -1,50 +1,46 @@
 <template>
-   <section class="bg-alt">
-      <div
-         class="py-15 lg:gap-8:py-20 mx-auto grid max-w-screen-xl gap-10 px-4 lg:grid-cols-12 xl:gap-0"
-      >
-         <div class="place-self-center lg:col-span-7 lg:mr-auto">
-            <h2
-               class="text-accent-secondary font-poppins-rounded mb-6 text-center text-2xl font-medium"
+   <div class="grid gap-10 lg:grid-cols-12 lg:gap-8 xl:gap-0">
+      <div class="place-self-center lg:col-span-7 lg:mr-auto">
+         <h2
+            class="text-accent-secondary font-poppins-rounded mb-6 text-center text-2xl font-medium"
+         >
+            Welcome to my lab
+         </h2>
+         <h1
+            class="font-poppins text-main mb-8 text-center text-5xl font-extrabold leading-none md:text-6xl lg:text-7xl"
+         >
+            Hi, I'm Simone!
+         </h1>
+         <p
+            class="text-main font-poppins mb-6 max-w-screen-md text-center text-lg leading-10 md:text-xl lg:mb-8 lg:text-2xl"
+         >
+            I'm a
+            <span class="font-poppins-rounded text-accent-primary font-medium"
+               >Full-Stack Software Engineer</span
             >
-               Welcome to my lab
-            </h2>
-            <h1
-               class="font-poppins text-main mb-8 text-center text-5xl font-extrabold leading-none md:text-6xl lg:text-7xl"
+            who's always tinkering with new ideas
+            <!-- and pushing the boundaries of what's possible in development -->
+            .
+            <br />
+            This portfolio is where I document my creative process, discoveries, and occasional
+            misadventures.
+         </p>
+         <div class="group flex justify-center">
+            <a
+               href="#"
+               class="border-inverse shadow-inverse bg-accent-secondary group-hover:!bg-accent-secondary-dark font-poppins-rounded inline-flex items-center justify-center gap-2 rounded border-[1.5px] p-4 px-8 text-white transition-colors"
             >
-               Hi, I'm Simone!
-            </h1>
-            <p
-               class="text-main font-poppins mb-6 max-w-screen-md text-center text-lg leading-10 md:text-xl lg:mb-8 lg:text-2xl"
-            >
-               I'm a
-               <span class="font-poppins-rounded text-accent-primary font-medium"
-                  >Full-Stack Software Engineer</span
-               >
-               who's always tinkering with new ideas
-               <!-- and pushing the boundaries of what's possible in development -->
-               .
-               <br />
-               This portfolio is where I document my creative process, discoveries, and occasional
-               misadventures.
-            </p>
-            <div class="group flex justify-center">
-               <a
-                  href="#"
-                  class="border-inverse shadow-inverse bg-accent-secondary group-hover:!bg-accent-secondary-dark font-poppins-rounded inline-flex items-center justify-center gap-2 rounded border-[1.5px] p-4 px-8 text-white transition-colors"
-               >
-                  Get in touch
-                  <i class="pi pi-bolt"></i>
-               </a>
-            </div>
-         </div>
-         <div class="hidden items-center justify-center lg:col-span-5 lg:flex">
-            <img
-               src="/images/avatar-transparent-cut.png"
-               alt="avatar"
-               class="border-inverse border-b-[1.5px]"
-            />
+               Get in touch
+               <i class="pi pi-bolt"></i>
+            </a>
          </div>
       </div>
-   </section>
+      <div class="hidden items-center justify-center lg:col-span-5 lg:flex">
+         <img
+            src="/images/avatar-transparent-cut.png"
+            alt="avatar"
+            class="border-inverse border-b-[1.5px]"
+         />
+      </div>
+   </div>
 </template>
