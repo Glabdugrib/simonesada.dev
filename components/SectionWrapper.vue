b7a0adb277da44d9f9bd5a00416d2587632de3c0
Simone Sada <si.sada93@gmail.com>
2025-11-04 17:56:07 +0100

    feat(sections): add section-wrapper component

---
+++ components/SectionWrapper.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+   id: string;
+}>();
+</script>
+
+<template>
+   <section :id>
+      <div class="mx-auto max-w-screen-xl items-center px-4 py-16 lg:px-6 lg:py-24">
+         <div class="debu g">
+            <slot />
+         </div>
+      </div>
+   </section>
+</template>
