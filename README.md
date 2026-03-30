a312c7e483d02020c8b31bbee204fe2acdbab2a6
Simone Sada <si.sada93@gmail.com>
2026-03-30 22:59:16 +0200

    style: formatting

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # simonesada.dev
 
-[//]: # (// x-release-please-start-version)
+[//]: # '// x-release-please-start-version'
+
 ![Latest Release](https://img.shields.io/badge/latest_release-amber?style=plastic&label=1.3.0)
 
 [//]: # '// x-release-please-end'
--- components/MarqueeDivider.vue
+++ components/MarqueeDivider.vue
@@ -48,7 +48,7 @@ onMounted(() => {
 </script>
 
 <template>
-   <section class="border-inverse border-y-2 bg-yellow-200">
+   <section class="border-inverse bg-alt border-y-2">
       <div
          ref="marqueeRef"
          class="relative flex overflow-hidden py-1 opacity-0 transition-opacity duration-150"
