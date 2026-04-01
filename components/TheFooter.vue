b93d9e02ad1e4df0140a9700d652aa367533b8c0
Simone Sada <70369064+Glabdugrib@users.noreply.github.com>
2026-04-01 10:24:18 +0200

    chore(main): release simonesada.dev 1.5.2 (#11)

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -3,7 +3,7 @@ import Logo from './Logo.vue';
 import { socialLinks } from '../config/appConfig';
 
 // x-release-please-start-version
-const appVersion = '1.5.1';
+const appVersion = '1.5.2';
 // x-release-please-end
 </script>
 
